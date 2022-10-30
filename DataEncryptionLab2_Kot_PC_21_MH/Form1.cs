abce0fd8471d5da9633ed531c619a230c986932c
Maksym Kot <redkotyara@yahoo.com>
2022-10-30 22:52:18 +0200

    Logic for decrypting and displaying numbers in different formats is added

--- DataEncryptionLab2_Kot_PC_21_MH/Form1.cs
+++ DataEncryptionLab2_Kot_PC_21_MH/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DataEncryptionLab2_Kot_PC_21_MH
@@ -23,7 +16,19 @@ namespace DataEncryptionLab2_Kot_PC_21_MH
             var integerKey = ReturnBinaryNumber(textBox_Key);
 
             var integerC = integerP ^ integerKey;
-            textBox_C.Text = Convert.ToString(integerC, 2);
+            textBox1.Text = textBox_C.Text = Convert.ToString(integerC, 2);
+
+            DrawNumberInAllFormats(label7, label8, label9, integerP);
+            DrawNumberInAllFormats(label10, label11, label12, integerKey);
+            DrawNumberInAllFormats(label13, label14, label15, integerC);
+
+            var decryptedPNumber = integerC ^ integerKey;
+            textBox3.Text = Convert.ToString(decryptedPNumber, 2);
+            textBox2.Text = textBox_Key.Text;
+
+            DrawNumberInAllFormats(label16, label17, label18, decryptedPNumber);
+            DrawNumberInAllFormats(label19, label20, label21, integerKey);
+            DrawNumberInAllFormats(label22, label23, label24, integerC);
         }
 
         private int ReturnBinaryNumber(TextBox textBox) => Convert.ToInt32(textBox.Text, 2);
@@ -31,6 +36,23 @@ namespace DataEncryptionLab2_Kot_PC_21_MH
         private void button1_Click(object sender, EventArgs e)
         {
             textBox_C.Text = textBox_Key.Text = textBox_P.Text = "";
+
+            label7.Text = label8.Text = label9.Text = "";
+            label10.Text = label11.Text = label12.Text = "";
+            label13.Text = label14.Text = label15.Text = "";
+
+            textBox1.Text = textBox2.Text = textBox3.Text = "";
+
+            label16.Text = label17.Text = label18.Text = "";
+            label19.Text = label20.Text = label21.Text = "";
+            label22.Text = label23.Text = label24.Text = "";
+        }
+
+        private void DrawNumberInAllFormats(Label decimalLabel, Label binaryLabel, Label hexLabel, int binaryNumber)
+        {
+            binaryLabel.Text = Convert.ToString(binaryNumber, 2);
+            hexLabel.Text = Convert.ToString(binaryNumber, 16).ToUpper();
+            decimalLabel.Text = Convert.ToString(binaryNumber, 10);
         }
     }
 }
