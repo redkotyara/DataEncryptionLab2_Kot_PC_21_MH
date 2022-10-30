e8508adf9d0da582614732dc77c8bc7abbeef9d6
Maksym Kot <redkotyara@yahoo.com>
2022-10-30 22:02:30 +0200

    Logic for binary numbers is added

--- DataEncryptionLab2_Kot_PC_21_MH/Form1.Designer.cs
+++ DataEncryptionLab2_Kot_PC_21_MH/Form1.Designer.cs
@@ -28,13 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_P = new System.Windows.Forms.TextBox();
+            this.textBox_Key = new System.Windows.Forms.TextBox();
+            this.textBox_C = new System.Windows.Forms.TextBox();
+            this.xor = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "P =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(145, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Key =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(156, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C =";
+            // 
+            // textBox_P
+            // 
+            this.textBox_P.Location = new System.Drawing.Point(198, 104);
+            this.textBox_P.Name = "textBox_P";
+            this.textBox_P.Size = new System.Drawing.Size(100, 20);
+            this.textBox_P.TabIndex = 3;
+            // 
+            // textBox_Key
+            // 
+            this.textBox_Key.Location = new System.Drawing.Point(198, 131);
+            this.textBox_Key.Name = "textBox_Key";
+            this.textBox_Key.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Key.TabIndex = 4;
+            // 
+            // textBox_C
+            // 
+            this.textBox_C.Enabled = false;
+            this.textBox_C.Location = new System.Drawing.Point(198, 166);
+            this.textBox_C.Name = "textBox_C";
+            this.textBox_C.Size = new System.Drawing.Size(100, 20);
+            this.textBox_C.TabIndex = 5;
+            // 
+            // xor
+            // 
+            this.xor.Location = new System.Drawing.Point(318, 129);
+            this.xor.Name = "xor";
+            this.xor.Size = new System.Drawing.Size(75, 23);
+            this.xor.TabIndex = 6;
+            this.xor.Text = "XOR";
+            this.xor.UseVisualStyleBackColor = true;
+            this.xor.Click += new System.EventHandler(this.xor_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(198, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 24);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(647, 329);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.xor);
+            this.Controls.Add(this.textBox_C);
+            this.Controls.Add(this.textBox_Key);
+            this.Controls.Add(this.textBox_P);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_P;
+        private System.Windows.Forms.TextBox textBox_Key;
+        private System.Windows.Forms.TextBox textBox_C;
+        private System.Windows.Forms.Button xor;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DataEncryptionLab2_Kot_PC_21_MH/Form1.cs
+++ DataEncryptionLab2_Kot_PC_21_MH/Form1.cs
@@ -16,5 +16,21 @@ namespace DataEncryptionLab2_Kot_PC_21_MH
         {
             InitializeComponent();
         }
+
+        private void xor_Click(object sender, EventArgs e)
+        {
+            var integerP = ReturnBinaryNumber(textBox_P);
+            var integerKey = ReturnBinaryNumber(textBox_Key);
+
+            var integerC = integerP ^ integerKey;
+            textBox_C.Text = Convert.ToString(integerC, 2);
+        }
+
+        private int ReturnBinaryNumber(TextBox textBox) => Convert.ToInt32(textBox.Text, 2);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox_C.Text = textBox_Key.Text = textBox_P.Text = "";
+        }
     }
 }
